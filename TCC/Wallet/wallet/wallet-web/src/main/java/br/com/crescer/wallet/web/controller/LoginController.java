cf2ee01d987a8f2cd749006c29589576650d2cba
Hedo <hedojunior@gmail.com>
2016-06-27 19:34:58 -0300

    feat(LoginController): adicionar controller de login.

---
+++ TCC/Wallet/wallet/wallet-web/src/main/java/br/com/crescer/wallet/web/controller/LoginController.java
@@ -0,0 +1,27 @@
+package br.com.crescer.wallet.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ *
+ * @author Hedo
+ */
+@Controller
+@RequestMapping(value = "/")
+public class LoginController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    String toLogin(Model model){
+        
+        return "login";
+    }
+    
+    @RequestMapping(value ="entrar", method = RequestMethod.POST)
+    String toHome(String inputUsuario, String inputSenha){
+        
+        return "home";
+    }
+}
