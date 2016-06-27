281236d75fcaeac1e6f9551598a7333c478aa9b4
Victor Ribeiro <victor.ribeiro@cwi.com.br>
2016-06-27 17:15:02 -0300

    feat(Teste): classes de teste apenas para criação de diretorios

---
+++ TCC/Wallet/wallet/wallet-entity/src/main/java/br/com/crescer/wallet/entity/teste.java
@@ -0,0 +1,13 @@
+/*
+ *  Crescer-TCC: Wallet
+ *  by: Douglas Ballester, Hedo Eccker e Victor Comette.
+ */
+package br.com.crescer.wallet.entity;
+
+/**
+ *
+ * @author victor.ribeiro
+ */
+public class teste {
+    
+}
---
+++ TCC/Wallet/wallet/wallet-service/src/main/java/br/com/crescer/wallet/service/teste.java
@@ -0,0 +1,13 @@
+/*
+ *  Crescer-TCC: Wallet
+ *  by: Douglas Ballester, Hedo Eccker e Victor Comette.
+ */
+package br.com.crescer.wallet.service;
+
+/**
+ *
+ * @author victor.ribeiro
+ */
+public class teste {
+    
+}
