7859e9214afb9b080567fe03409b79f8963ac068
Douglas Valle Ballester <douglas.ballester@cwinet.local>
2016-06-27 18:01:39 -0300

    feat(Repository): criar repositorio para serviço e usuario

---
+++ TCC/Wallet/wallet/wallet-service/src/main/java/br/com/crescer/wallet/service/repository/ServicoRepository.java
@@ -0,0 +1,14 @@
+package br.com.crescer.wallet.service.repository;
+
+import br.com.crescer.wallet.entity.Servico;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author douglas.ballester
+ */
+
+
+public interface ServicoRepository extends CrudRepository<Servico, Long> {
+    
+}
