2d37b783bc5d080bf54cdbed0f63757e533c1ad4
Hedo <hedojunior@gmail.com>
2016-06-28 11:35:45 -0300

    feat(UsuarioService): adicionar serviço de Usuário.

---
+++ TCC/Wallet/wallet/wallet-service/src/main/java/br/com/crescer/wallet/service/service/UsuarioService.java
@@ -0,0 +1,22 @@
+package br.com.crescer.wallet.service.service;
+
+import br.com.crescer.wallet.entity.Usuario;
+import br.com.crescer.wallet.service.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author Hedo
+ */
+
+@Service
+public class UsuarioService {
+    
+    @Autowired
+    UsuarioRepository repository;
+    
+    public Usuario findOneByDsUserName(String dsUserName){
+        return repository.findOneByDsUserName(dsUserName);
+    }
+}
