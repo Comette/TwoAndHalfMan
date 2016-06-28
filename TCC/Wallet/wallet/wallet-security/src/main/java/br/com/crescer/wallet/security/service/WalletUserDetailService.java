3f1fb58667d8d5f28f6b2ec31cad99f84792991b
Hedo <hedojunior@gmail.com>
2016-06-28 11:35:07 -0300

    feat(security): adicionar busca ao banco na autenticação.

--- TCC/Wallet/wallet/wallet-security/src/main/java/br/com/crescer/wallet/security/service/WalletUserDetailService.java
+++ TCC/Wallet/wallet/wallet-security/src/main/java/br/com/crescer/wallet/security/service/WalletUserDetailService.java
@@ -4,12 +4,14 @@
  */
 package br.com.crescer.wallet.security.service;
 
+import br.com.crescer.wallet.entity.Usuario;
 import br.com.crescer.wallet.security.enumeration.WalletRoles;
+import br.com.crescer.wallet.service.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 /**
@@ -17,14 +19,20 @@ import org.springframework.stereotype.Service;
  * @author victor.ribeiro
  */
 @Service
-public class WalletUserDetailService implements UserDetailsService{
+public class WalletUserDetailService implements UserDetailsService {
+
     private static final String CRESCER = "crescer";
+    @Autowired
+    UsuarioService service;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        if (username.isEmpty()) {
+        Usuario user = service.findOneByDsUserName(username);
+
+        if (username.isEmpty() || user == null) {
             throw new UsernameNotFoundException(String.format("User with username=%s was not found", username));
+        } else {
+            return new User(user.getDsUserName(), user.getDsSenha(), WalletRoles.valuesToList());
         }
-        return new User(username, new BCryptPasswordEncoder().encode(CRESCER), WalletRoles.valuesToList());
     }
 }
