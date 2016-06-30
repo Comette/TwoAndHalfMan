a948b7d361488e83a1bbef8479c12693ec6d81ff
Hedo <hedojunior@gmail.com>
2016-06-30 16:30:44 -0300

    feat(GRAFICO): finalmente implementar o gráfico corretamente.

--- TCC/Wallet/wallet/wallet-web/src/main/resources/static/js/graphs.js
+++ TCC/Wallet/wallet/wallet-web/src/main/resources/static/js/graphs.js
@@ -1,6 +1,21 @@
 function renderizarGrafico($container, listaServicos) {
     var labels = [];
     var data = [];
+    var colors = [
+        '#FBAF41',
+        '#B0B5B6',
+        '#434343',
+        '#D1603D',
+        '#FCB97D',
+        '#EDD892',
+        '#272838',
+        '#E5E059',
+        '#BDD358',
+        '#6D213C',
+        '#946846',
+        '#729B79',
+        '#9F7E69'
+    ];
     $.each(listaServicos, function (i, servico) {
         labels[i] = servico.nome;
         data[i] = accounting.toFixed(servico.porcentagemCustoTotal, 2);
@@ -8,39 +23,26 @@ function renderizarGrafico($container, listaServicos) {
 
     var ctx = $container;
     var grafico = new Chart(ctx, {
-        type: 'bar',
+        type: 'pie',
+        animation: {
+            animateRotate: true
+        },
         data: {
             labels: labels,
             datasets: [{
-                    label: 'Custo Mensal',
                     data: data,
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 1)'
-                    ],
-                    borderColor: [
-                        'rgba(255,99,132,1)'
-                    ],
-                    borderWidth: 1
+                    backgroundColor: colors
                 }]
-        },
-        options: {
-            scales: {
-                yAxes: [{
-                        ticks: {
-                            beginAtZero: true
-                        }
-                    }]
-            }
         }
     });
 }
 
-function buscarDadosEChamarGraficos($container1, $container2){
+function buscarDadosEChamarGraficos($container1, $container2) {
     $.ajax({
-       url: '/servicos-inflar-grafico',
-       type: 'GET'
-    }).done(function(data){
-        renderizarGrafico($container1,data.servicosDesteMes);
+        url: '/servicos-inflar-grafico',
+        type: 'GET'
+    }).done(function (data) {
+        renderizarGrafico($container1, data.servicosDesteMes);
         renderizarGrafico($container2, data.servicosProximoMes);
     });
 }
