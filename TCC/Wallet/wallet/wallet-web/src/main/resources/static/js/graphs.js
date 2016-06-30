c7b05a842051274ffe61972fa6040ec775279e73
Hedo <hedojunior@gmail.com>
2016-06-30 16:58:32 -0300

    feat(graphs.js): adicionar método gerador de cores para o gráfico.

--- TCC/Wallet/wallet/wallet-web/src/main/resources/static/js/graphs.js
+++ TCC/Wallet/wallet/wallet-web/src/main/resources/static/js/graphs.js
@@ -1,21 +1,16 @@
 function renderizarGrafico($container, listaServicos) {
     var labels = [];
     var data = [];
-    var colors = [
-        '#FBAF41',
-        '#B0B5B6',
-        '#434343',
-        '#D1603D',
-        '#FCB97D',
-        '#EDD892',
-        '#272838',
-        '#E5E059',
-        '#BDD358',
-        '#6D213C',
-        '#946846',
-        '#729B79',
-        '#9F7E69'
-    ];
+    var colors = [];
+    
+    
+    var randomColorGenerator = function () { 
+    return '#' + (Math.random().toString(16) + '0000000').slice(2, 8); 
+};
+    $.each(listaServicos, function(i){
+       colors[i] = randomColorGenerator(); 
+    });
+    
     $.each(listaServicos, function (i, servico) {
         labels[i] = servico.nome;
         data[i] = accounting.toFixed(servico.porcentagemCustoTotal, 2);
