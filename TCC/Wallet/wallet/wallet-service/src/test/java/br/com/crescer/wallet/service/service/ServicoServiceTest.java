cf962e09079a3373c50973ddd1789d20fc2f0ae2
Douglas Ballester <douglas.ballester@cwi.com>
2016-07-01 04:22:47 -0300

    fix(ServicoService): fazer verificacao se servico recebido por parametro é null

--- TCC/Wallet/wallet/wallet-service/src/test/java/br/com/crescer/wallet/service/service/ServicoServiceTest.java
+++ TCC/Wallet/wallet/wallet-service/src/test/java/br/com/crescer/wallet/service/service/ServicoServiceTest.java
@@ -117,7 +117,6 @@ public class ServicoServiceTest {
             doReturn(EMPTY_LIST).when(repository).findByDsSituacaoNot(any(Situacao.class), any(Pageable.class));
             doReturn(EMPTY_LIST).when(repository).findByDsSituacaoNot(any(Situacao.class));
 
-
             assertTrue(service.geraDadosDashboard(new PageRequest(1, 1)).getServicosMesAtual().isEmpty());
             assertTrue(service.geraDadosDashboard(new PageRequest(1, 1)).getServicosProximoMes().isEmpty());
             assertEquals(BigDecimal.ZERO, service.geraDadosDashboard(new PageRequest(1, 1)).getGastoTotalAtual());
@@ -240,6 +239,7 @@ public class ServicoServiceTest {
             assertEquals(BigDecimal.valueOf(100).setScale(6, HALF_UP), service.getDadosGraficoServicos().getServicosProximoMes().get(0).getPorcentagemCustoTotal());
         }
     }
+
     @Test
     public void testGetServicosDTOProximoMesFiltradosPorGerentePaginados() {
         {
@@ -274,6 +274,13 @@ public class ServicoServiceTest {
 
     @Test
     public void testGetServicoDTO() {
-
+        {
+            doReturn(mockServico).when(repository).findOne(any(Long.class));
+            assertEquals(service.getServicoDTO(1l).getNome(), "servico");
+        }
+        {
+            doReturn(null).when(repository).findOne(any(Long.class));
+            assertNull(service.getServicoDTO(1l));
+        }
     }
 }
