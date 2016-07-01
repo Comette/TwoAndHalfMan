c63dc367310abe4fdcc0899acd6c166e7b33d39d
Comette <victor.ribeiro@cwi.com.br>
2016-07-01 04:25:44 -0300

    refactor(CotacaoService, ServicoService): removidos imports desnecessários

--- TCC/Wallet/wallet/wallet-service/src/test/java/br/com/crescer/wallet/service/service/CotacaoServiceTest.java
+++ TCC/Wallet/wallet/wallet-service/src/test/java/br/com/crescer/wallet/service/service/CotacaoServiceTest.java
@@ -7,7 +7,6 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
