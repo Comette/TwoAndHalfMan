3ac7093b3651da66f8e4c03b08378dab989461e0
Victor Ribeiro <victor.ribeiro@cwi.com.br>
2016-07-01 16:57:09 -0300

    feat(404) criada pagina 404 custom;

--- TCC/Wallet/wallet/wallet-web/src/main/java/br/com/crescer/wallet/AppRun.java
+++ TCC/Wallet/wallet/wallet-web/src/main/java/br/com/crescer/wallet/AppRun.java
@@ -6,6 +6,11 @@ package br.com.crescer.wallet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.ErrorPage;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
@@ -15,7 +20,18 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class AppRun {
+
     public static void main(String[] args) throws Exception {
         SpringApplication.run(AppRun.class, args);
+
+    }
+
+    @Bean
+    public EmbeddedServletContainerCustomizer containerCustomizer() {
+
+        return (ConfigurableEmbeddedServletContainer container) -> {
+            ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/404.html");
+            container.addErrorPages(error404Page);
+        };
     }
 }
