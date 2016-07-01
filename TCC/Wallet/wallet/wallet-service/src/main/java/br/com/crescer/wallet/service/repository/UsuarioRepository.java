d0583b74e06906029d93fc79bcd131f83e7c8216
Douglas Ballester <douglas.ballester@cwi.com>
2016-07-01 16:55:29 -0300

    fix(UsuarioService, UsuarioRepository): alterar metodo para que no filtro  por gerente, não mostre gerentes inativos

--- TCC/Wallet/wallet/wallet-service/src/main/java/br/com/crescer/wallet/service/repository/UsuarioRepository.java
+++ TCC/Wallet/wallet/wallet-service/src/main/java/br/com/crescer/wallet/service/repository/UsuarioRepository.java
@@ -1,7 +1,9 @@
 package br.com.crescer.wallet.service.repository;
 
 import br.com.crescer.wallet.entity.Permissao;
+import br.com.crescer.wallet.entity.Situacao;
 import br.com.crescer.wallet.entity.Usuario;
+import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 
 /**
@@ -13,5 +15,5 @@ import org.springframework.data.repository.CrudRepository;
 public interface UsuarioRepository extends CrudRepository<Usuario, Long> {
     
     public Usuario findUsuarioByDsUserName(String dsUserName);
-    public Iterable<Usuario> findAllBytpPermissao(Permissao permissao);
+    public List<Usuario> findAllByTpPermissaoAndDsSituacaoNot(Permissao permissao, Situacao situacao);
 }
