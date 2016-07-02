bed4840660a01031494098f43f5c1e87ddf40251
Hedo <hedojunior@gmail.com>
2016-07-02 20:06:21 -0300

    fix(cadastro.js): mudar função para que troque as tabs baseado nas strings 'idUsuario' e 'idServico'

--- TCC/Wallet/wallet/wallet-web/src/main/resources/static/js/cadastro.js
+++ TCC/Wallet/wallet/wallet-web/src/main/resources/static/js/cadastro.js
@@ -2,7 +2,8 @@
 
 $(function () {
     var url = document.location.toString();
-    $('.nav-tabs a[href="#' + url.split('#')[1] + '"]').tab('show');
+    var str = url.substring(url.indexOf("?") + 1, url.indexOf("="));
+    $('.nav-tabs a[href="#' + str + '"]').tab('show');
 
     $('.nav-tabs a').on('shown.bs.tab', function (e) {
         window.location.hash = e.target.hash;
