db29af15273c1b31bf78085dec2ae74cd9b95192
Hedo <hedojunior@gmail.com>
2016-07-05 17:33:49 -0300

    feat(wallet): adicionar correções de bugs e remoção de deleção própria.

--- TCC2/Wallet/wallet/wallet-web/target/classes/static/js/usuarios.js
+++ TCC2/Wallet/wallet/wallet-web/target/classes/static/js/usuarios.js
@@ -2,21 +2,23 @@
 
 var $containerUsuarios1 = $('#lista-gerentes-1');
 var $containerUsuarios2 = $('#lista-gerentes-2');
+var idUsuario;
 
 $(function () {
+    idUsuario = $('#inputIdUsuario').val();
     fazerRequestUsuarios();
 });
 
 var appendarUsuariosNaLista = function(usuarios, $lista) {
     $lista.html('');
     $.each(usuarios, function (i, usuario) {
-        var $listGroup = $('<a href="/usuario?idUsuario=' + usuario.id + '" class="list-group-item">');
+        var $listGroup = $('<a href="/usuario?idClient=' + usuario.id + '" class="list-group-item">');
         var $buttonExcluir = $('<button class="btn btn-danger glyphicon glyphicon-remove" name="btn-excluir-gerente" value="' + usuario.id + '">');
         var $small = $('<small>').text('( ' + usuario.permission + ' ) ').attr('style', 'font-weight: bold; margin-left: 10px;');
-        var $btnEditar = $('<a class="btn btn-warning glyphicon glyphicon-pencil" name="btn-editar-gerente" href="/editar-usuario?idUsuario=' + usuario.id + '">');
+        var $btnEditar = $('<a class="btn btn-warning glyphicon glyphicon-pencil" name="btn-editar-gerente" href="/editar-usuario?idClient=' + usuario.id + '">');
         var $h4 = $('<h4>').text(usuario.username);
-
-        if (usuario.situacao === 'INATIVO')
+        debugger;
+        if (usuario.id === parseInt(idUsuario))
             $lista.append($listGroup.append($btnEditar).append($h4).append($small));
         else
             $lista.append($listGroup.append($btnEditar).append($h4).append($buttonExcluir).append($small));
